ec5661344a706c4915c7e671fd71444ac8455548
LanceOP <49013973+LanceOP@users.noreply.github.com>
2020-11-29 11:14:36 +0530

    delete1

--- aware/android/app/src/main/kotlin/com/example/aware/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aware
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- aware/lib/monitor/constantsResultWindow.dart
+++
@@ -1,9 +0,0 @@
-class Constants{
-  static const String History = 'History';
-  static const String Analysis = 'Analysis';
-
-  static const List<String> choices = <String>[
-    History,
-    Analysis,
-  ];
-}
